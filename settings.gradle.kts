f88d6d98b3f131359f36d753132453ea0267853e
WeddedMercury15 <96825350+WeddedMercury15@users.noreply.github.com>
2024-11-14 13:46:35 +0800

    Create settings.gradle.kts

---
+++ settings.gradle.kts
@@ -0,0 +1,13 @@
+rootProject.name = "RealisticVillagers"
+
+include(
+    "1_18_R2",
+    "1_19_R3",
+    "1_20_R1",
+    "1_20_R2",
+    "1_20_R3",
+    "1_20_R4",
+    "1_21_R1",
+    "core",
+    "dist"
+)
